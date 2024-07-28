7f9c483566dabafe2f466e7a9058b314e9754e83
kfjatooshek <90kwiatkowskitomasz@gmail.com>
2024-07-28 19:42:24 +0200

    base.py added annotations to the __init__ for better code readability

--- dwarven_victory/base.py
+++ dwarven_victory/base.py
@@ -1,6 +1,11 @@
 class GameObject:
-    def __init__(self):
+    def __init__(self, name: str, avatar: str, hp: int) -> None:
         self.name = ""
         self.avatar = ""
         self.hp = 0
         self.strength = 0
+
+    def _str__(self):
+        return (f'{'Name: ':<30}{self.name:>20}\n'
+                f'{'Strength ':<30}{self.strength:>20}\n'
+                f'{'HP: ':<30}{self.hp:>20}\n')

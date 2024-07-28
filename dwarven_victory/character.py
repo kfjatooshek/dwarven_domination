41f7476939b8bf001ac4fa8f99a7eaaf47c3261e
kfjatooshek <90kwiatkowskitomasz@gmail.com>
2024-07-28 19:43:14 +0200

    base.py added annotations to the __init__ for better code readability

--- dwarven_victory/character.py
+++ dwarven_victory/character.py
@@ -1,18 +1,16 @@
 import random
-from avatars import avatars_design, avatars_logic
-import items
+from base import GameObject
+from dwarven_victory import avatars
+from dwarven_victory import items
+import items.items_logic as items
 
 
-class CreateCharacter:
+class CreateCharacter(GameObject):
     def __init__(self):
+        super().__init__()
         self.race = None
-        self.name = None
-        self.avatar = None
         self.exp = 0
         self.level = 0
-        self.HP = 1000  # +armor.HP[0] # TODO introduce armors (items.py armor function)
-        self.MP = 1000
-        self.strength = 100
         self.weapon_slots = 1
         self.armor_slots = 2
         self.weapon = None
@@ -20,31 +18,24 @@ class CreateCharacter:
         self.race_attributes = {}
 
     def __str__(self):
-        return (f'{'Name: ':<30}{self.name:>20}\n'
-                f'{'Race: ':<30}{self.race:>20}\n'
+        return (f'{'Race: ':<30}{self.race:>20}\n'
                 f'{'Experience: ':<30}{self.exp:>20}\n'
                 f'{'Level: ':<30}{self.level:>20}\n'
-                f'{'Strength ':<30}{self.strength:>20}\n'
-                f'{'HP: ':<30}{self.HP:>20}\n'
-                f'{'MP: ':<30}{self.MP:>20}\n\n'
                 f'{'This is how you look like:':<30}{self.avatar}\n\n'
                 f'{'Your weapon:':<30}{self.weapon[2]}\n{self.weapon[0]}')
-
 
     @staticmethod
     def chose_weapon():
         return items.pick_weapon()
 
     def adjust_character_stats(self):
-        self.HP = int(self.HP * random.uniform(self.race_attributes['hp_modifier'][0],
+        self.hp = int(self.HP * random.uniform(self.race_attributes['hp_modifier'][0],
                                                self.race_attributes['hp_modifier'][1]))
-        self.MP = int(self.MP * random.uniform(self.race_attributes['mp_modifier'][0],
-                                               self.race_attributes['mp_modifier'][1]))
         self.strength = int(self.strength * random.uniform(self.race_attributes['str_modifier'][0],
                                                            self.race_attributes['str_modifier'][1]))
 
     def got_hit(self):
-        self.HP = self.HP-100
+        self.hp = self.hp-100
 
 
 class DwarfCharacter(CreateCharacter):
@@ -55,7 +46,6 @@ class DwarfCharacter(CreateCharacter):
         self.name = input("Please enter your character's name:")
         self.avatar = avatars_logic.pick_avatar(avatars_design.dwarf_avatars)
         self.race_attributes = {'hp_modifier': (1, 2),
-                                'mp_modifier': (0.1, 0.2),
                                 'str_modifier': (1, 2)}
         self.adjust_character_stats()
         self.weapon = items.pick_weapon()
@@ -66,3 +56,5 @@ class ElvenCharacter(CreateCharacter):
     # container for future development of another class
     pass
 
+x = DwarfCharacter
+print(x)

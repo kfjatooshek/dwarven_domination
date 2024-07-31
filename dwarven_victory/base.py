cc923d104c6ed06ef3fa249360e550c4b75af544
kfjatooshek <90kwiatkowskitomasz@gmail.com>
2024-07-31 15:32:47 +0200

    character_stats_dict structure was changed from function that returns a dictionary, to a dictionary, just unify this approach across the whole project

--- dwarven_victory/base.py
+++ dwarven_victory/base.py
@@ -1,7 +1,9 @@
 class GameObject:
-    def __init__(self, name: str = None, avatar: str = None) -> None:
+    def __init__(self, name: str = None, avatar: str = None, hp: int = 10, strength: int = 10) -> None:
         self.name = name
         self.avatar = avatar
+        self.hp = hp
+        self.str = strength
 
     def _str__(self):
         return (f'{'Name: ':<30}{self.name:>20}\n'
--- dwarven_victory/main.py
+++ dwarven_victory/main.py
@@ -1,7 +1,6 @@
 # main game loop
 from dwarven_victory.character.character import DwarvenCharacter, ElvenCharacter
 from dwarven_victory.avatars import avatars_logic
-from dwarven_victory.character import character_stats_dict
 from dwarven_victory.character.character_logic import pick_race
 
 
@@ -12,12 +11,14 @@ def main():
 
     players_avatar = avatars_logic.pick_avatar(players_race)
 
+    players_character = None
     if players_race == "dwarf":
-        players_character = DwarvenCharacter(name=character_name, race=players_race, avatar=players_avatar )
+        players_character = DwarvenCharacter(name=character_name, avatar=players_avatar)
     if players_race == "elf":
-        players_character = ElvenCharacter(name=character_name, race=players_race, avatar=players_avatar)
+        players_character = ElvenCharacter(name=character_name, avatar=players_avatar)
 
     print(players_character)
+    print(players_character.strength, players_character.race, players_character.hp)
 
 
 if __name__ == "__main__":
